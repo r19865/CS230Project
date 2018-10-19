cf24beaec6d473418584a70b08c8ea98ff909dcc
Reina Ishibashi <rishibashi@carroll.edu>
2018-10-19 14:36:01 -0600

    updated tile class

--- tile.java
+++ tile.java
@@ -5,22 +5,19 @@ public class tile
 	BufferedImage image;
 	String type;
 	Boolean onBoard;
-	Boolean playable;
 	
 	tile()
 	{
 		image=null;
 		type=null;
 		onBoard=false;
-		playable=false;
 	}
 	
-	tile (BufferedImage myImage, String tileType, Boolean isItOnBoard, Boolean isItPlayable)
+	tile (BufferedImage myImage, String tileType, Boolean isItOnBoard)
 	{
 		this.image=myImage;
 		this.type=tileType;
 		this.onBoard=isItOnBoard;
-		this.playable=isItPlayable;
 	}
 	
 	tile (tile t)
@@ -28,7 +25,6 @@ public class tile
 		this.image=t.image;
 		this.type=t.type;
 		this.onBoard=t.onBoard;
-		this.playable=t.playable;
 	}
 	
 	public BufferedImage getImage()
@@ -46,11 +42,6 @@ public class tile
 		return onBoard;
 	}
 	
-	public Boolean getPlayable()
-	{
-		return playable;
-	}
-	
 	public void setImage(BufferedImage newImage)
 	{
 		image= newImage;
@@ -66,14 +57,10 @@ public class tile
 		onBoard=newOnBoard;
 	}
 	
-	public void setPlayable(Boolean newPlayable)
-	{
-		playable=newPlayable;
-	}
 	
 	public String toString()
 	{
-		return String.format("Tile Type: %s%n Is it Playable?: %s%n Is it on the board? %s%n ", type, onBoard,playable);
+		return String.format("Tile Type: %s%n Is it on the board? %s%n ", type, onBoard);
 	}
 	
 	public int compareTo(tile other)
